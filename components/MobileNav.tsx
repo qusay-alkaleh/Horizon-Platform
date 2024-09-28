e36e05a11fbfb40bc7a1f738db0a21a1831c25a8
KusayAlkaleh <theonexr4@gmail.com>
2024-09-28 03:59:35 +0300

    created "Footer" componets and added it in other components

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,9 +4,6 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { sidebarLinks } from "@/constants";
@@ -14,6 +11,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathName = usePathname();
@@ -83,7 +81,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
               })}
               USER
             </nav>
-            FOOTER
+            <Footer user={user} type="mobile" />
           </div>
         </SheetContent>
       </Sheet>
